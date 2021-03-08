df4eeacef0e459dc7b703a38b0543dfd4f6a5a91
adam-fraga <adam.fraga@laplateforme.io>
2021-03-08 16:07:01 +0100

    Nombre premier done

--- jour01/job08/script.js
+++ jour01/job08/script.js
@@ -1,3 +1,19 @@
+/**
+ * Test if 2 number are prime and display the sum of both
+ * @param a
+ * @param b
+ */
 function sommenombrespremiers(a, b) {
 
+    // Function test if a number is prime or not
+    let isPrime = function (nb) {
+        if (nb < 2) return false;
+        for (let i = 2; i <= Math.sqrt(nb); i++)
+            if (nb % i === 0) return false;
+        return true;
+    }
+    // Display sum of two prime number or false
+    if (isPrime(a) === true && isPrime(b) === true) {
+        console.log(a + b)
+    } else console.log(false)
 }
