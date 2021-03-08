32981d6837edd8af04db1339d1974c0a40b19256
adam-fraga <adam.fraga@laplateforme.io>
2021-03-08 17:51:54 +0100

    delete consigne

--- jour01/job09/script.js
+++ jour01/job09/script.js
@@ -1,9 +1,3 @@
-// Créez une fonction “tri” qui prend en paramètres un tableau de nombres
-// nommé “numbers” et une variable “order” qui contient “asc” ou “desc”. A
-// l’aide d’un algorithme, cette fonction doit trier le tableau dans l’ordre
-// ascendant ou décroissant, selon le paramètre passé, puis retourner le
-// tableau
-
 /**
  * Sort a Table of numbers acording to user prefere asc or desc
  * @param {array} numbers
