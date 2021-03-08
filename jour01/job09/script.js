d0cfd9a2043da47ec91b10350e9908bc866cc589
adam-fraga <adam.fraga@laplateforme.io>
2021-03-08 19:35:30 +0100

    last job done

---
+++ jour01/job09/script.js
@@ -0,0 +1,31 @@
+/**
+ * Function sort table of number asc or desc acording to user's choice
+ * @param numbers
+ * @param order
+ * @returns {boolean}
+ */
+function tri(numbers, order) {
+    let tmp
+    if (order === 'asc') {
+        for (let i = 0; i < numbers.length; i++) {
+            for (let j = 0; j < numbers.length; j++) {
+                if (numbers[j] > numbers[i]) {
+                    tmp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = tmp;
+                }
+            }
+        }
+        console.log(numbers)
+    } else if (order === 'desc') {
+        for (let i = 0; i < numbers.length; i++) {
+            for (let j = 0; j < numbers.length; j++) {
+                if (numbers[j] < numbers[i]) {
+                    tmp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = tmp;
+                }
+            }
+        }
+    } else return false
+}
