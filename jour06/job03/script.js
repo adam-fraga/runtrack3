d078a9aa654d6285a00f67807b19ffce30d33b90
adam-fraga <adam.fraga@laplateforme.io>
2021-03-15 18:23:23 +0100

    job03 done

---
+++ jour06/job03/script.js
@@ -0,0 +1,43 @@
+// Media queries
+let largeScreen = window.matchMedia("(min-width:1600px)");
+let mediumScreen = window.matchMedia("(min-width:760px)");
+let smallScreen = window.matchMedia("(max-width:759px)");
+
+//
+function responsive(e) {
+    if (largeScreen.matches) {
+        console.log('LARGE')
+        $('main').css({
+            "display": "flex",
+            "flex-direction":"row"
+        })
+        $('.section').css({
+            "width": "23%",
+            "height": "200px",
+        })
+    } else if (mediumScreen.matches) {
+        console.log('MEDIUM')
+        $('main').css({
+            "display": "flex",
+            "flex-direction":"row",
+            "flex-wrap": "wrap"
+        })
+        $('.section').css({
+            "width": "45%",
+            "height": "300px",
+        })
+    } else if (smallScreen.matches) {
+        console.log('SMALL')
+        $('main').css({
+            "display": "flex",
+            "flex-direction": "column",
+        })
+        $('main .section').css({
+            "width": "90%",
+            "height": "200px",
+            "align-self":"center"
+        })
+    }
+}
+
+window.addEventListener('resize', responsive)
