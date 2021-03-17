83438b30052f7fc5e5efb3bd94ccf85616f5f0bb
adam-fraga <adam.fraga@laplateforme.io>
2021-03-17 17:34:53 +0100

    revue job01

--- jour08/job01/index.php
+++ jour08/job01/index.php
@@ -8,14 +8,96 @@
     <title>Document</title>
 </head>
 <body>
-
+<header>
+    <nav>
+        <a href="#">Accueil</a>
+        <a href="#">Inscription</a>
+        <a href="#">Connexion</a>
+        <a href="#">Rechercher</a>
+    </nav>
+</header>
+<main>
+    <section>
+        <form action="" method="post">
+            <!--            CIVILITE-->
+            <fieldset>
+                <legend>Civilité</legend>
+                <div>
+                    <input type="radio" id="femme">
+                    <label for="femme">Femme</label>
+                </div>
+                <div>
+                    <input id="homme" type="radio">
+                    <label for="homme">Homme</label>
+                </div>
+            </fieldset>
+            <!--            INFOS PERSO-->
+            <fieldset>
+                <legend>Informations personnels</legend>
+                <div>
+                    <div>
+                        <label for="nom">Nom</label>
+                        <input type="text" name="nom" id="nom">
+                    </div>
+                    <div>
+                        <label for="prenom">Prénom</label>
+                        <input type="text" id="prenom" name="prenom">
+                    </div>
+                    <div>
+                        <label for="adress">Adresse</label>
+                        <input id="adress" type="text" name="adresse">
+                    </div>
+                    <div>
+                        <div>
+                            <p>Passions:</p>
+                        </div>
+                        <div>
+                            <label for="informatique">Informatique</label>
+                            <input id="informatique" type="checkbox" name="informatique">
+                        </div>
+                        <div>
+                            <label for="sport">Sport</label>
+                            <input id="sport" type="checkbox" name="sport">
+                        </div>
+                        <div>
+                            <label for="voyage">Voyages</label>
+                            <input id="voyage" type="checkbox" name="voyage">
+                        </div>
+                        <div>
+                            <label for="lecture">Lecture</label>
+                            <input id="lecture" type="checkbox" name="lecture">
+                        </div>
+                    </div>
+                </div>
+            </fieldset>
+            <fieldset>
+                <legend>Informations de connexion</legend>
+                <div>
+                    <div>
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email">
+                    </div>
+                    <div>
+                        <label for="password">Mot de passe</label>
+                        <input type="password" id="password" name="password">
+                    </div>
+                    <div>
+                        <label for="confPassword">Confirmer mot de passe</label>
+                        <input type="password" id="confPassword" name="confPassword">
+                    </div>
+                    <div>
+                        <label for="adress">Adresse</label>
+                        <input id="adress" type="text" name="adresse">
+                    </div>
+                </div>
+            </fieldset>
+            <div>
+                <button type="submit" id="button">Envoyer</button>
+            </div>
+        </form>
+    </section>
+</main>
 <footer>
 </footer>
-<!-- Compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-<!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-
 </body>
 </html>
